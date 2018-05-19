39ead9a31b4a4e966a70182f8f80ea280ef9c5a5
panda2048 <panda2048@protonmail.com>
2018-05-19 09:36:56 +0000

    latest

--- bot.js
+++ bot.js
@@ -16,86 +16,115 @@ const nodemailer = require('nodemailer')
 const APIKEY = 'xxx'
 const APISECRET = 'xxx'
 
-const tracked_max = 1000
+const tracked_max = 200
+const wait_time = 150 			// ms
+
 const depth_limit = 10
-const wait_time = 1000 			// ms
+
+const M = 100000000
+
+let isReady = false
 
 let pairs = []
-
-let depth_bids = {}
-let depth_asks = {}
+let ticksize = []
 
 let vol = []
 
+let bidask = []
+
 let minute_prices = {}
-let avgBuyVol = []
-let avgBuyVol2 = []
+let ema = []
+let stoch_k = []
+let stoch_kx = []
+let stoch_d = []
+let fisher_n = []
+let fisher = []
+
+const ema_period = 200
+const k = 14
+const kx = 3
+const d = 3
+const fisher_period = 10
 
 let isTrading = []
-let metFirstTarget = []
-let initVol = []
 let buyingPrice = []
-let tradingTime = []
+let buyingTime = []
+let stopgain = []
+let stoploss = []
+
+let signal1 = []
 
 let totalProfit = 0
+
+let numBuy = 0
+let maxBuy = 0
 
 //////////////////////////////////////////////////////////////////////////////////
 // that's where you define your buying conditions:
 //////////////////////////////////////////////////////////////////////////////////
 
-bump_detect = (pair, tick) => {
-	let { t:time, o:open, h:high, l:low, c:close, v:volume, n:trades, i:interval, x:isFinal, q:quoteVolume, V:buyVolume, Q:quoteBuyVolume } = tick
-	let buyPercentVol = quoteBuyVolume / quoteVolume 
-	let averageBuyVol = (quoteBuyVolume - avgBuyVol[pair])/avgBuyVol[pair]
-	let averageBuyVol2 = (quoteBuyVolume - avgBuyVol2[pair])/avgBuyVol2[pair]
-	let priceDiff = (close - minute_prices[pair][0][4])/minute_prices[pair][0][4]
-	//console.log(buyPercentVol + "," + averageBuyVol + "," + priceDiff)
-	if (!isTrading[pair]) {
-		if (!metFirstTarget[pair]) {
-			if (isFinal && buyPercentVol > 0.7 && averageBuyVol > 1 && averageBuyVol2 > 1 && priceDiff >= 0.002 && quoteBuyVolume >= vol[pair]/200) {
-				console.log(pair + " 1 > " + buyPercentVol + "," + averageBuyVol + "," + averageBuyVol2 + "," + priceDiff + "," + quoteBuyVolume)
-				metFirstTarget[pair] = true
-				initVol[pair] = quoteBuyVolume
-			}
-		} else {
-			if (buyPercentVol > 0.7 && quoteBuyVolume >= minute_prices[pair][0][10]*0.5 && averageBuyVol > 1 && averageBuyVol2 > 1 && priceDiff >= 0 && quoteBuyVolume >= initVol[pair]*1.4) {
-				console.log(pair + " 2.1 > " + buyPercentVol + "," + averageBuyVol + "," + averageBuyVol2 + "," + priceDiff + "," + quoteBuyVolume)
-				metFirstTarget[pair] = false
-				tradingTime[pair] = time
+function buying(pair, candlesticks) {
+	if (!isReady) return
+
+	let { e:eventType, E:eventTime, s:symbol, k:ticks } = candlesticks
+	let { t:time, o:_open, h:_high, l:_low, c:_close, v:volume, n:trades, i:interval, x:isFinal, q:quoteVolume, V:buyVolume, Q:quoteBuyVolume } = ticks
+
+	let open = (_open*M).toFixed(0)
+	let high = (_high*M).toFixed(0)
+	let low = (_low*M).toFixed(0)
+	let close = (_close*M).toFixed(0)
+
+	let _ema = (_close - ema[symbol])*(2/(ema_period+1))+ema[symbol]
+	let max_high = Math.max(_high, Math.max.apply(null, minute_prices[symbol].slice(0,k-1).map(tick => tick[2])))
+	let min_low = Math.min(_low, Math.min.apply(null, minute_prices[symbol].slice(0,k-1).map(tick => tick[3])))
+	let _k = ((_close - min_low)/(max_high - min_low))*100
+	let _kx = (_k + stoch_k[symbol].slice(0,kx-1).reduce((sum, price) => (sum + parseFloat(price)), 0)) / kx
+	let _d = (_kx + stoch_kx[symbol].slice(0,d-1).reduce((sum, price) => (sum + parseFloat(price)), 0)) / d
+
+	let hl2 = (parseFloat(_high)+parseFloat(_low))/2
+	let max_hl2 = Math.max(hl2, Math.max.apply(null, minute_prices[symbol].slice(0,fisher_period-1).map(tick => (parseFloat(tick[2])+parseFloat(tick[3]))/2)))
+	let min_hl2 = Math.min(hl2, Math.min.apply(null, minute_prices[symbol].slice(0,fisher_period-1).map(tick => (parseFloat(tick[2])+parseFloat(tick[3]))/2)))
+	
+	let _fisher_n = 0.33*2*((hl2-min_hl2)/(max_hl2-min_hl2)-0.5)+0.67*fisher_n[symbol]
+	let x = _fisher_n>0.99?0.99999999:_fisher_n<-0.99?-0.99999999:_fisher_n
+	let _fisher = 0.5*Math.log((1+x)/(1-x))+0.5*fisher[symbol]
+	
+	if (!isTrading[pair] && isFinal) {
+		if (_fisher <= fisher[symbol]) { 
+			signal1[symbol] = true
+		}
+		if (signal1[symbol] && _close >= _ema) {
+			if (fisher[symbol] < -2) console.log(pair + " > 1.0 > " + _fisher + "," + fisher[symbol])
+			if (fisher[symbol] < -2 && _fisher < -2 && _fisher > fisher[symbol] && _kx >= 20 && _kx <= 70 && quoteVolume/vol[symbol] >= 0.005) {
+				console.log(pair + " > 1.1 > " + _fisher + "," + fisher[symbol])
+				console.log(pair + " > 1.2 > " + quoteBuyVolume/quoteVolume + "," + quoteBuyVolume/vol[symbol] + "," + quoteVolume/vol[symbol])
+
 				return "BUY"
-			} else if (isFinal) {
-				console.log(pair + " 2.2 > " + buyPercentVol + "," + averageBuyVol + "," + averageBuyVol2 + "," + priceDiff + "," + quoteBuyVolume)
-				console.log(pair + " 2.2 > reset")
-				metFirstTarget[pair] = false
 			}
 		}
-		return "HOLD"
-	} else {
-		if (quoteBuyVolume >= initVol[pair]*1.4) {
-			console.log(pair + " 3.1 > " + buyPercentVol + "," + averageBuyVol + "," + averageBuyVol2 + "," + priceDiff + "," + quoteBuyVolume)
-			return "SELL"
-		}
-		if (tradingTime[pair] != time) {
-			if (quoteBuyVolume >= minute_prices[pair][0][10]*0.6 && priceDiff <= -0.004) {
-				console.log(pair + " 3.2 > " + buyPercentVol + "," + averageBuyVol + "," + averageBuyVol2 + "," + priceDiff + "," + quoteBuyVolume)
-				return "SELL"
-			} 
-			if (isFinal) {
-				if (quoteBuyVolume >= minute_prices[pair][0][10]*0.6) {
-					console.log(pair + " 3.3 > " + buyPercentVol + "," + averageBuyVol + "," + averageBuyVol2 + "," + priceDiff + "," + quoteBuyVolume)
-					return "HOLD"
-				} else {
-					console.log(pair + " 3.4 > " + buyPercentVol + "," + averageBuyVol + "," + averageBuyVol2 + "," + priceDiff + "," + quoteBuyVolume)
-					return "SELL"
-				}
-			}
+		if (_fisher > fisher[symbol]) { 
+			signal1[symbol] = false
 		}
 	}
 }
 
-let strategies = [ 
-	{ name: "BUMP", condition: bump_detect }, 
-]
+function selling(pair, ts) {
+	if (!isReady) return
+	
+	if (isTrading[pair]) {
+		let _stopgain = buyingPrice[pair] + Math.round(buyingPrice[pair]*0.005)
+		let _stoploss = buyingPrice[pair] - Math.round(buyingPrice[pair]*0.0025)
+		if (bidask[pair][0][0][0] >= _stopgain) {
+			console.log(pair + " > 2.1 > " + _stopgain)
+			signal1[pair]=false
+			return "SELL"
+		} else if (bidask[pair][0][0][0] <= _stoploss) {
+			console.log(pair + " > 2.2 > " + _stoploss)
+			signal1[pair]=false
+			return "SELL"
+		}
+	}
+}
 
 //////////////////////////////////////////////////////////////////////////////////
 
@@ -106,8 +135,6 @@ console.log('------------ NBT starting -------------')
 
 async function run() {
 
-	await sleep(2)
-
 	console.log('------------------------------')
 	console.log(' get_BTC_pairs start')
 	console.log('------------------------------')
@@ -116,16 +143,17 @@ async function run() {
 	pairs = pairs.slice(0, tracked_max) //for debugging purpose
 	console.log("Total BTC pairs: " + pairs.length)
 	console.log('------------------------------')
-	
-	await sleep(2)
+/*
+  //await get_ticksize()
+	await sleep(10000)
 
 	console.log('------------------------------')
 	console.log(' trackVolumeData start')
 	console.log('------------------------------')
 	await trackVolumeData()
 	console.log('------------------------------')
-
-	await sleep(2)
+	
+	await sleep(10000)
 
 	console.log('------------------------------')
 	console.log(' trackMinutePrices start')
@@ -133,6 +161,15 @@ async function run() {
 	await trackMinutePrices()
 	console.log('------------------------------')
 
+	await sleep(10000)
+*/
+	console.log('------------------------------')
+	console.log(' trackDepthData start')
+	console.log('------------------------------')
+	await trackDepthData()
+	console.log('------------------------------')
+
+  //isReady = true
 	console.log('------------ we are ready to track all strategies -------------')
 }
 
@@ -143,22 +180,37 @@ sleep = (x) => {
 }
 
 function filterPairs(pair) {
-	if (pair.symbol.endsWith('BTC') && pair.quoteVolume >= 400 && pair.quoteVolume <= 99999) {
+	if (pair.symbol.endsWith('BTC') && pair.prevClosePrice > 0) {
     		return true;
 	} 
 	return false; 
 }
 
 get_BTC_pairs = () => {
+  return new Promise(resolve => {
+    binance.prevDay(false, (error, data) => {
+      if (error) {
+        console.log( error )
+        resolve([])
+      }
+      if (data) {
+        resolve( data.filter(filterPairs).map(pair=>pair.symbol) )
+      }
+    })
+  })
+}
+
+get_ticksize = () => {
 	return new Promise(resolve => {
-		binance.prevDay(false, (error, data) => {
-			if (error) {
-				console.log( error )
-				resolve([])
+		binance.exchangeInfo((error, data) => {
+			for ( let obj of data.symbols ) {
+				for ( let filter of obj.filters ) {
+					if ( filter.filterType == "PRICE_FILTER" ) {
+						ticksize[obj.symbol] = filter.tickSize*M;
+					}
+				}
 			}
-			if (data) {
-				resolve( data.filter(filterPairs).map(pair=>pair.symbol) )
-			}
+			resolve(true)
 		})
 	})
 }
@@ -181,30 +233,55 @@ async function trackVolumeData() {
 	for (var i = 0, len = pairs.length; i < len; i++) {
 		var pair = pairs[i]
 		trackVolumePair(pair)
+		await sleep(wait_time)
+	}
+}
+
+async function trackDepthData() {
+	for (var i = 0, len = pairs.length; i < len; i++) {
+		await trackDepthPair(pairs[i])
+		await sleep(wait_time)
 	}
 }
 
 trackDepthPair = (pair) => {
 	return new Promise(resolve => {
-		console.log( "> starting tracking depth data for " + pair )
-		binance.websockets.depthCache([pair], (symbol, depth) => {
-			var bids = binance.sortBids(depth.bids, depth_limit)
-			var asks = binance.sortAsks(depth.asks, depth_limit)
-			depth_asks[pair] = _.sum(_.values(asks).slice(0,depth_limit))*binance.first(asks)
-			depth_bids[pair] = _.sum(_.values(bids).slice(0,depth_limit))*binance.first(bids)
-			depth_diff[pair] = 100 * (binance.first(asks) - binance.first(bids)) / (binance.first(bids))
-		}, depth_limit);
-		resolve(true)
-	}, depth_limit)
-}
+		let symbol = pair
+		binance.websockets.depth([pair], (depth) => {
+/*			let _b = [] 
+			let _a = []
+			let bids = binance.sortBids(depth.bids, depth_limit)
+			let asks = binance.sortAsks(depth.asks, depth_limit)
+		
+			for (let o = 0; o < depth_limit; o++) {
+				_b[o] = []
+				_b[o][0] = parseFloat(_.keys(bids)[o])*M
+				_b[o][1] = parseFloat(_.values(bids)[o])
+				_a[o] = []
+				_a[o][0] = parseFloat(_.keys(asks)[o])*M
+				_a[o][1] = parseFloat(_.values(asks)[o])
+			}
+			bidask[symbol] = []
+			bidask[symbol][0] = _b
+			bidask[symbol][1] = _a
 
-async function trackDepthData() {
-	for (var i = 0, len = pairs.length; i < len; i++) {
-		var pair = pairs[i]
-		await trackDepthPair(pair)
-		await sleep(wait_time)
-		console.log( (i+1) + " > " + pair + " depth tracked a:" + numeral(depth_asks[pair]).format("0.00") + " / b:" + numeral(depth_bids[pair]).format("0.00") )
-	}
+			if (isTrading[pair]) {
+				var ts = (new Date()).getTime()
+				let cond = selling(symbol, ts)
+					if (cond === "SELL") {
+					isTrading[pair] = false
+					numBuy--
+					console.log(symbol + " SELL - " + bidask[symbol][0][0][0] + " - " + numBuy + "," + maxBuy)
+					let profit = ((bidask[symbol][0][0][0] - buyingPrice[pair]) / buyingPrice[pair]) - 0.001
+					totalProfit += profit
+					console.log(symbol + " > Profit: " + profit + " / Net: " + totalProfit)
+				}
+			}
+*/	  
+			resolve(true)
+		})
+		resolve(true)
+	})
 }
 
 getPrevMinutePrices = (pair) => {
@@ -216,25 +293,71 @@ getPrevMinutePrices = (pair) => {
 			}
 			if (ticks) {
 				minute_prices[symbol] = _.drop(_.reverse( ticks )) 
-				avgBuyVol[symbol] = minute_prices[symbol].map(tick => tick[10]).reduce((sum, price) => (sum + parseFloat(price)), 0) / minute_prices[symbol].length
-				avgBuyVol2[symbol] = minute_prices[symbol].slice(0,6).map(tick => tick[10]).reduce((sum, price) => (sum + parseFloat(price)), 0) / minute_prices[symbol].length
-				//console.log(avgBuyVol[symbol])
+				// EMA
+				ema[symbol] = minute_prices[symbol].slice(ema_period).map(tick => tick[4]).reduce((sum, price) => (sum + parseFloat(price)), 0) / ema_period
+
+				for (let i=ema_period-1; i >= 0; i--) {
+					ema[symbol] = (minute_prices[symbol][i][4] - ema[symbol])*(2/(ema_period+1))+ema[symbol]
+				}
+			
+				//Stochastic
+				stoch_k[symbol] = []
+				stoch_kx[symbol] = []
+				stoch_d[symbol] = []
+				for (let j=ema_period-k; j>=0; j--) {
+					//console.log(minute_prices[symbol].slice(j,j+k))
+					let max_high = Math.max.apply(null, minute_prices[symbol].slice(j,j+k).map(tick => tick[2]))
+					let min_low = Math.min.apply(null, minute_prices[symbol].slice(j,j+k).map(tick => tick[3]))
+					//console.log(max_high, min_low)
+					stoch_k[symbol].unshift(((minute_prices[symbol][j][4] - min_low)/(max_high - min_low))*100)
+					if (stoch_k[symbol].length >= kx) {
+						if (stoch_k[symbol].length > kx) stoch_k[symbol].pop()
+						//console.log("k " + stoch_k[symbol])
+						stoch_kx[symbol].unshift(stoch_k[symbol].reduce((sum, price) => (sum + parseFloat(price)), 0) / kx)
+						if (stoch_kx[symbol].length >= d) {
+							if (stoch_kx[symbol].length > d) stoch_kx[symbol].pop()
+							stoch_d[symbol].pop()
+							//console.log("kx " + stoch_kx[symbol])
+							stoch_d[symbol].unshift(stoch_kx[symbol].reduce((sum, price) => (sum + parseFloat(price)), 0) / d)
+							//console.log("d " + stoch_d[symbol])
+						}
+					}
+				}
+				//console.log(stoch_kx[symbol][0])
+				//console.log(stoch_d[symbol][0])
+
+				// fisher
+				fisher_n[symbol] = 0
+				fisher[symbol] = 0
+				for (let j=ema_period-fisher_period; j>=0; j--) {
+					let hl2 = (parseFloat(minute_prices[symbol][j][2])+parseFloat(minute_prices[symbol][j][3]))/2
+					let max_hl2 = Math.max.apply(null, minute_prices[symbol].slice(j,j+fisher_period).map(tick => (parseFloat(tick[2])+parseFloat(tick[3]))/2))
+					let min_hl2 = Math.min.apply(null, minute_prices[symbol].slice(j,j+fisher_period).map(tick => (parseFloat(tick[2])+parseFloat(tick[3]))/2))
+					
+					fisher_n[symbol] = 0.33*2*((hl2-min_hl2)/(max_hl2-min_hl2)-0.5)+0.67*fisher_n[symbol]
+					let x = fisher_n[symbol]>0.99?0.99999999:fisher_n[symbol]<-0.99?-0.99999999:fisher_n[symbol]
+					fisher[symbol] = 0.5*Math.log((1+x)/(1-x))+0.5*fisher[symbol]
+				}
+				//console.log(fisher[symbol])
+
 				resolve(true)
 			}
-		}, {limit:13})
+		}, {limit:ema_period*2+1})
 	})
 }
 
 async function trackMinutePrices() {
 	for (var i = 0, len = pairs.length; i < len; i++) {
 		isTrading[pairs[i]] = false
-		metFirstTarget[pairs[i]] = false
+		stopgain[pairs[i]] = 0
+		stoploss[pairs[i]] = 0
+		signal1[pairs[i]] = false
 		//warmup
 		await getPrevMinutePrices(pairs[i])
-		//await sleep(wait_time)
+		await sleep(wait_time)
 		console.log( (i+1) + " > " + pairs[i] + " " + minute_prices[pairs[i]].length + " unit prices retrieved")
 		await trackFutureMinutePrices(pairs[i])
-		//await sleep(wait_time)
+		await sleep(wait_time)
 		console.log( (i+1) + " > " + pairs[i] + " future prices tracked.")
 	}
 }
@@ -243,34 +366,30 @@ trackFutureMinutePrices = (pair) => {
 	return new Promise(resolve => {
 		binance.websockets.candlesticks([pair], "5m", (candlesticks) => {
 			let { e:eventType, E:eventTime, s:symbol, k:ticks } = candlesticks
-			let { t:time, o:open, h:high, l:low, c:close, v:volume, n:trades, i:interval, x:isFinal, q:quoteVolume, V:buyVolume, Q:quoteBuyVolume } = ticks
-			//console.log("> new tick for pair: " + pair)
-			//console.log(ticks)
-			strategies.map( strat => {
-				if (!isTrading[pair]) {
-					if (strat.condition(symbol, ticks) === "BUY") {
-						isTrading[pair]=true
-						buyingPrice[pair] = close
-						console.log(symbol + " BUY - " + close + " = " + vol[symbol])
-					}	
-				} else {
-					if (strat.condition(symbol, ticks) === "SELL") {
-						isTrading[pair]=false
-						console.log(symbol + " SELL - " + close + " - " + vol[symbol])
-						let profit = ((close - buyingPrice[pair]) / buyingPrice[pair]) - 0.001
-						totalProfit += profit
-						console.log(symbol + " > Profit: " + profit + " / Net: " + totalProfit)
-					} 
-				}
-			})
+			let { t:time, o:_open, h:_high, l:_low, c:_close, v:volume, n:trades, i:interval, x:isFinal, q:quoteVolume, V:buyVolume, Q:quoteBuyVolume } = ticks
+			
+			let open = (_open*M).toFixed(0)
+			let high = (_high*M).toFixed(0)
+			let low = (_low*M).toFixed(0)
+			let close = (_close*M).toFixed(0)
+			
+			let cond = buying(symbol, candlesticks)
+			if (cond === "BUY" && !isTrading[pair]) {
+				isTrading[pair]=true
+				buyingPrice[pair] = bidask[symbol][1][0][0]
+				buyingTime[pair] = eventTime
+				numBuy++
+				if (numBuy > maxBuy) maxBuy = numBuy
+				console.log(symbol + " BUY - " + close + "," + bidask[symbol][1][0][0] + " - " + numBuy + "," + maxBuy)
+			}	
+			
 			if (isFinal) {
-				//console.log("update 5m")
 				let tick = minute_prices[symbol].pop()
 				tick[0] = time
-				tick[1] = open
-				tick[2] = high
-				tick[3] = low
-				tick[4] = close
+				tick[1] = _open
+				tick[2] = _high
+				tick[3] = _low
+				tick[4] = _close
 				tick[5] = volume
 				tick[6] = 0
 				tick[7] = quoteVolume
@@ -278,10 +397,36 @@ trackFutureMinutePrices = (pair) => {
 				tick[9] = buyVolume
 				tick[10] = quoteBuyVolume 
 				minute_prices[symbol].unshift(tick)
-				//console.log(minute_prices[symbol])
-				avgBuyVol[symbol] = minute_prices[symbol].map(tick => tick[10]).reduce((sum, price) => (sum + parseFloat(price)), 0) / minute_prices[symbol].length
-				avgBuyVol2[symbol] = minute_prices[symbol].slice(0,6).map(tick => tick[10]).reduce((sum, price) => (sum + parseFloat(price)), 0) / minute_prices[symbol].length
-				//console.log(avgBuyVol[symbol])
+				ema[symbol] = (minute_prices[symbol][0][4] - ema[symbol])*(2/(ema_period+1))+ema[symbol]
+				//console.log(ema[symbol])
+
+				let max_high = Math.max.apply(null, minute_prices[symbol].slice(0,k).map(tick => tick[2]))
+				let min_low = Math.min.apply(null, minute_prices[symbol].slice(0,k).map(tick => tick[3]))
+				stoch_k[symbol].unshift(((minute_prices[symbol][0][4] - min_low)/(max_high - min_low))*100)
+				if (stoch_k[symbol].length >= kx) {
+					if (stoch_k[symbol].length > kx) stoch_k[symbol].pop()
+					//console.log("k " + stoch_k[symbol])
+					stoch_kx[symbol].unshift(stoch_k[symbol].reduce((sum, price) => (sum + parseFloat(price)), 0) / kx)
+					if (stoch_kx[symbol].length >= d) {
+						if (stoch_kx[symbol].length > d) stoch_kx[symbol].pop()
+						stoch_d[symbol].pop()
+						//console.log("kx " + stoch_kx[symbol])
+						stoch_d[symbol].unshift(stoch_kx[symbol].reduce((sum, price) => (sum + parseFloat(price)), 0) / d)
+						//console.log("d " + stoch_d[symbol])
+					}
+				}
+
+				let hl2 = (parseFloat(minute_prices[symbol][0][2])+parseFloat(minute_prices[symbol][0][3]))/2
+				let max_hl2 = Math.max.apply(null, minute_prices[symbol].slice(0,fisher_period).map(tick => (parseFloat(tick[2])+parseFloat(tick[3]))/2))
+				let min_hl2 = Math.min.apply(null, minute_prices[symbol].slice(0,fisher_period).map(tick => (parseFloat(tick[2])+parseFloat(tick[3]))/2))
+					
+				fisher_n[symbol] = 0.33*2*((hl2-min_hl2)/(max_hl2-min_hl2)-0.5)+0.67*fisher_n[symbol]
+				let x = fisher_n[symbol]>0.99?0.99999999:fisher_n[symbol]<-0.99?-0.99999999:fisher_n[symbol]
+				fisher[symbol] = 0.5*Math.log((1+x)/(1-x))+0.5*fisher[symbol]
+				
+				//console.log(stoch_kx[symbol][0])
+				//console.log(stoch_d[symbol][0])
+				//console.log(fisher[symbol])
 			}
 		});
 		resolve(true)
